65bbceb32faadb8e4c58647383b6b43d22764f3d
WATA-Yiju <yiju.dev@watanow.com>
2024-04-13 23:14:25 +0900

    feat(atom): RestaurantHeaderRow 컴포넌트 구현

---
+++ react-app/src/components/atom/RestaurantHeaderRow.tsx
@@ -0,0 +1,11 @@
+function RestaurantHeaderRow() {
+  return (
+    <tr>
+      <th style={{ paddingRight: '48px' }}>식당 이름</th>
+      <th style={{ paddingRight: '48px' }}>종류</th>
+      <th>메뉴</th>
+    </tr>
+  );
+}
+
+export default RestaurantHeaderRow;
