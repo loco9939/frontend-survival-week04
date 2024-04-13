20acf0d97dd3644a2aeb14344f9319ca84bfe2de
WATA-Yiju <yiju.dev@watanow.com>
2024-04-13 23:18:49 +0900

    feat(App): App 컴포넌트 구현

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+import { useBoolean, useLocalStorage } from 'usehooks-ts';
+import FilterableRestaurantsTable from './components/organism/FilterableRestaurantsTable';
+import Receipt from './components/organism/Receipt';
+import SelectedMenu from './components/organism/SelectedMenu';
+import { ReceiptType } from './types/receipt';
+import { MenuType } from './types/restaurant';
+
 export default function App() {
+  const { value: showReceiptState, setFalse: hideReceipt, setTrue: showReceipt } = useBoolean();
+
+  const [selectedMenu, setSelectedMenu] = useLocalStorage<MenuType[]>('selectedMenu', []);
+  const [receipt, setReceipt] = useState<ReceiptType|null>(null);
   return (
-    <p>
-      과제를 진행해 주세요.
-    </p>
+    <>
+      <h1>푸드코트 키오스크</h1>
+
+      <SelectedMenu
+        selectedMenu={selectedMenu}
+        setSelectedMenu={setSelectedMenu}
+        setReceipt={setReceipt}
+        showReceipt={showReceipt}
+      />
+
+      <FilterableRestaurantsTable setSelectedMenu={setSelectedMenu} />
+
+      {showReceiptState ? (
+        <Receipt
+          receipt={receipt}
+          showReceiptState={showReceiptState}
+          hideReceipt={hideReceipt}
+        />
+      ) : (
+        <p>[영수증 나오는 곳]</p>
+      )}
+
+    </>
   );
 }
