3c15a1c9ef9063d5e11b1b7c1fc58fc81cc71678
WATA-Yiju <yiju.dev@watanow.com>
2024-04-13 23:18:03 +0900

    feat(organism): Receipt 컴포넌트 구현

---
+++ react-app/src/components/organism/Receipt.tsx
@@ -0,0 +1,50 @@
+import { useInterval } from 'usehooks-ts';
+import { ReceiptType } from '../../types/receipt';
+
+type ReceiptProps = {
+  receipt:ReceiptType|null
+  showReceiptState:boolean
+  hideReceipt:() => void
+}
+
+function Receipt({
+  receipt,
+  showReceiptState,
+  hideReceipt,
+}: ReceiptProps) {
+  useInterval(() => {
+    hideReceipt();
+  }, showReceiptState ? 5000 : null);
+  return (
+    <div style={{ width: '500px', textAlign: 'center', border: '1px solid black' }}>
+      <h2>영수증</h2>
+
+      <div>
+        <h3>주문번호</h3>
+        <p>{receipt?.id}</p>
+      </div>
+
+      <div>
+        <h3>주문목록</h3>
+        <ul style={{ listStyle: 'none', padding: 0 }}>
+          {receipt?.menu.map((menu) => (
+            <li key={menu.id} style={{ marginBlock: '8px' }}>
+              {menu.name}
+              (
+              {menu.price.toLocaleString('ko-kr')}
+              원)
+            </li>
+          ))}
+
+        </ul>
+        <p>
+          총 가격:
+          {receipt?.totalPrice.toLocaleString('ko-kr')}
+          원
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Receipt;
