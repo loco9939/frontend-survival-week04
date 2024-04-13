93029e64ff84fae007ce23d08989e95b7c26d096
WATA-Yiju <yiju.dev@watanow.com>
2024-04-13 23:15:00 +0900

    feat(molecule): FilterCategory 컴포넌트 구현

---
+++ react-app/src/components/molecule/FilterCategory.tsx
@@ -0,0 +1,36 @@
+import { Category } from '../../types/category';
+
+type FilterCategoryProps = {
+  categories:Category[]
+  selectedCategory:Category
+  setSelectedCategory:(category:Category) => void
+}
+
+function FilterCategory({
+  categories,
+  selectedCategory,
+  setSelectedCategory,
+
+}: FilterCategoryProps) {
+  return (
+    <div style={{ marginBlock: '16px' }}>
+      {categories.map((category) => (
+        <button
+          key={category}
+          type="button"
+          style={{
+            marginRight: '12px',
+            color: selectedCategory === category
+              ? 'red'
+              : '',
+          }}
+          onClick={() => setSelectedCategory(category)}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default FilterCategory;
